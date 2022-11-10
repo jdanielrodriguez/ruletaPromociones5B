78db98a47d550d768ef7c6915f8012955a22971a
jdanielrodriguez <daniel.rodriguez@vidoomy.com>
2022-11-10 03:46:02 -0600

    ajusted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,32 +10,32 @@ declare var $: any
 export class AppComponent {
   title = 'ruleta5b';
   closeResult = '';
+  private modalService!: NgbModal;
+  constructor() { }
 
-	constructor(private modalService: NgbModal) {}
-
-	open(content: any) {
-		this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', fullscreen: 'modal-fullscreen-xl-down' }).result.then(
-			(result) => {
-				this.closeResult = `Closed with: ${result}`;
-			},
-			(reason) => {
-				this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-			},
-		);
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', fullscreen: 'modal-fullscreen-xl-down' }).result.then(
+      (result: any) => {
+        this.closeResult = `Closed with: ${result}`;
+      },
+      (reason: any) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      },
+    );
     console.log(this.closeResult);
-	}
+  }
 
-	close(content: any) {
-		this.modalService.dismissAll();
-	}
+  close(content: any) {
+    this.modalService.dismissAll();
+  }
 
-	private getDismissReason(reason: any): string {
-		if (reason === ModalDismissReasons.ESC) {
-			return 'by pressing ESC';
-		} else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-			return 'by clicking on a backdrop';
-		} else {
-			return `with: ${reason}`;
-		}
-	}
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
